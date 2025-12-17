a918647a5d47a428c7e8123921c3cdeffbbdd247
Dionisio G. Noriega Alvarez <NoriegaADG@outlook.com>
2025-12-16 23:28:12 -0600

    Se agregan rutsa absolutas

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Spinner } from '@core';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text> HOLA MUNDIO DESDE REACT</Text>
       <StatusBar style="auto" />
+
+      <Spinner /> 
+
     </View>
   );
 }
